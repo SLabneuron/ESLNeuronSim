8f73e0b2de6e64d88770a1a60d96bef7148de88c
SFuji <shirafujilab@gmail.com>
2023-11-30 15:26:38 +0900

    make controller file

---
+++ SLabBCSwitch.py
@@ -0,0 +1,30 @@
+"""
+coding: utf-8
+
+Created on Thu Nov 30 13:52
+
+@author: SLab
+
+BCSwitch controller
+"""
+
+#Libralies
+import tkinter as tk
+from tkinter import ttk
+
+#Local Libralies
+
+
+
+class BCSwitch:
+    def __init__(self):
+        print("success")
+        pass
+
+
+if __name__ == "__main__":
+    ins = BCSwitch()
+    
+
+
+
