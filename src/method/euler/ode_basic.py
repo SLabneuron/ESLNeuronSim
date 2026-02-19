c45c378852791ddbb88d49c83a7ef2c85c717164
SFuji <shirafujilab@gmail.com>
2026-02-19 11:01:16 +0900

    update readme

--- src/method/euler/ode_basic.py
+++ src/method/euler/ode_basic.py
@@ -23,14 +23,14 @@ from numba import njit
 
 
 @njit
-def calc_time_evolution_ode(init_x, init_y, h,
-                        tau1, b1, S, WE11, WE12, WI11, WI12,
-                        tau2, b2, WE21, WE22, WI21, WI22,
+def calc_time_evolution_ode(init_x, init_y, init_z, h,
+                            a, b, c, d, r, s, X_R, Input,
                         total_step, index_start, store_step):
 
     # variables
     x_next = x_previous = init_x
     y_next = y_previous = init_y
+    z_next = z_previous = init_z
 
     # time
     T = 0
@@ -39,18 +39,21 @@ def calc_time_evolution_ode(init_x, init_y, h,
     t_hist = np.zeros(store_step, dtype=np.float32)
     x_hist = np.zeros(store_step, dtype=np.float32)
     y_hist = np.zeros(store_step, dtype=np.float32)
+    z_hist = np.zeros(store_step, dtype=np.float32)
 
     idx = 0
 
     for i in range(total_step):
 
         # calculate
-        x_next = x_previous + h * (1 / tau1) * ((b1 * S + WE11 * x_previous**2 + WE12 * y_previous**2) * (1 - x_previous) - (1 + WI11 * x_previous**2 + WI12 * y_previous**2) * x_previous)
-        y_next = y_previous + h * (1 / tau2) * ((b2 * S + WE21 * x_previous**2 + WE22 * y_previous**2) * (1 - y_previous) - (1 + WI21 * x_previous**2 + WI22 * y_previous**2) * y_previous)
+        x_next = x_previous + h * (y_previous - a* x_previous**3 + b* x_previous**2 - z_previous + Input)
+        y_next = y_previous + h * (c - d*x_previous**2 - y_previous)
+        z_next = z_previous + h * (s*(x_previous - X_R) - z_previous)
 
         # update variables and time
         x_previous = x_next
         y_previous = y_next
+        z_previous = z_next
         T += h
 
         # store registers
@@ -63,9 +66,10 @@ def calc_time_evolution_ode(init_x, init_y, h,
                 t_hist[idx_insert] = T
                 x_hist[idx_insert] = x_previous
                 y_hist[idx_insert] = y_previous
+                z_hist[idx_insert] = z_previous
 
             idx += 1
 
-    return t_hist, x_hist, y_hist
+    return t_hist, x_hist, y_hist, z_hist
 
 
